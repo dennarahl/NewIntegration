1e9f89b018dcfc3791e7089a559e96ca15bfdef8
Shannon <shannonmvoth@gmail.com>
2024-03-09 21:42:50 -0700

    everything works!

--- lib/NostromoPinsAndGlobals.h
+++ lib/NostromoPinsAndGlobals.h
@@ -1,9 +1,10 @@
-//PIN DEFINITIONS
+
+#define NostromoPinsAndGlobals_h
+// PIN DEFINITIONS
 
 #define TRIGGER_PIN 13
 #define ECHO_PIN 14
 
-
-//GLOBAL DEFINITIONS
+// GLOBAL DEFINITIONS
 
 #define STOP_DISTANCE 30
